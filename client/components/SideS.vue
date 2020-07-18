8b524dcdf38ad9da2e23ac3727305cf6a214ab4f
Pirvan Marian <pirvan.marian@gmail.com>
2020-07-18 18:16:14 +0300

    'update-sidebar-menu'

--- client/components/SideS.vue
+++ client/components/SideS.vue
@@ -1,8 +1,5 @@
 <template>
 <div class="col-md-12">
-
-
- 
       <div class="px-3 py-2">
         <a href="/" css="logo">
                     <i class="fas fa-headphones"></i>
@@ -10,78 +7,120 @@
                 </a>
                 <ul class="list-home">
                     <li>
-                        <i class="fas fa-home"></i> Home
+                        <i class="fas fa-home"></i>  <a href="/">Home </a>
                     </li>
                 </ul>
 
                 <ul class="list-videos">
                     <li>
+                    <h3 class="title-menu">
                         <a href="/videos">
-                            <i class="fa fa-music"></i> Latest Videos
-                        </a>
+                            Videos
+                        </a></h3>
                     </li>
 
                     <li>
-                        <a href="/videos/ambiental">Ambiental Videos</a>
+                        <a href="/videos/ambiental">Ambiental </a>
                     </li>
                     <li>
-                        <a href="/videos/chillhop">ChillHop Videos</a>
+                        <a href="/videos/chillhop">ChillHop </a>
                     </li>
                     <li>
-                        <a href="/videos/chillout">ChillOut Videos</a>
-                    </li>
-
-                    <li>
-                        <a href="/videos/chillstep">Chillstep Videos</a>
+                        <a href="/videos/chillout">ChillOut </a>
                     </li>
 
                     <li>
-                        <a href="/videos/classic">Classic Videos</a>
-                    </li>
-                    <li>
-                        <a href="/videos/classical">Classical Videos</a>
-                    </li>
-                    <li>
-                        <a href="/videos/downtempo">Downtempo Videos</a>
-                    </li>
-                    <li>
-                        <a href="/videos/lofi">Lofi Videos</a>
-                    </li>
-                    <li>
-                        <a href="/videos/lounge">Lounge Videos</a>
+                        <a href="/videos/chillstep">Chillstep </a>
                     </li>
 
                     <li>
-                        <a href="/videos/regional">Regional Videos</a>
+                        <a href="/videos/classic">Classic </a>
+                    </li>
+                    <li>
+                        <a href="/videos/classical">Classical </a>
+                    </li>
+                    <li>
+                        <a href="/videos/downtempo">Downtempo </a>
+                    </li>
+                    <li>
+                        <a href="/videos/lofi">Lofi </a>
+                    </li>
+                    <li>
+                        <a href="/videos/lounge">Lounge </a>
                     </li>
 
                     <li>
-                        <a href="/videos/trap">Trap Videos</a>
+                        <a href="/videos/regional">Regional </a>
                     </li>
 
                     <li>
-                        <a href="/videos/world">World Videos</a>
+                        <a href="/videos/trap">Trap </a>
+                    </li>
+
+                    <li>
+                        <a href="/videos/techno">Techno</a>
                     </li>
                 </ul>
 
+
+                 <ul class="list-playlist">
+                  <li>
+                        <a class="title-menu" href="/playlists">Playlists </a>
+                    </li>
+
+                       <li>
+                        <a class="" href="/playlists/1">Ambiental </a>
+                    </li>
+                      <li>
+                        <a class="" href="/playlists/4">ChillOut </a>
+                    </li>
+                      <li>
+                        <a class="" href="/playlists/6">ChillStep </a>
+                    </li>
+                      <li>
+                        <a class="" href="/playlists/3">ChillHop </a>
+                    </li>
+                      <li>
+                        <a class="" href="/playlists/7">Classic </a>
+                    </li>
+                      <li>
+                        <a class="" href="/playlists/8">Classical </a>
+                    </li>
+                      <li>
+                        <a class="" href="/playlists/9">DownTempo </a>
+                    </li>
+                      <li>
+                        <a class="" href="/playlists/10">Lofi </a>
+                    </li>
+                      <li>
+                        <a class="" href="/playlists/13">Lounge </a>
+                    </li>
+                      <li>
+                        <a class="" href="/playlists/20">Trap </a>
+                    </li>
+
+                     <li>
+                        <a class="" href="/playlists/19">Techno </a>
+                    </li>
+                 </ul>
                 <ul class="list-helpfull">
-                    <li>HelpFull</li>
+                    <li class="title-menu">HelpFull</li>
 
                     <li>Contact</li>
                     <li>Suport</li>
-                    <li>Suggestion</li>
+                    <li>  <a href="/suggest">Suggest </a></li>
                 </ul>
 
                 <ul class="social-list">
-                    <h1>Follow Us!</h1>
+                    <h1 class="title-menu">Follow Us!</h1>
                     <li>
                         <a href="https://twitter.com/chill_whisper">
-                            <i class="fab fa-twitter"></i>
+                            <i class="fab fa-twitter">Twitter</i>
                         </a>
                     </li>
                     <li>
                         <a href="https://www.facebook.com/Chill-Whisper-317341858891377/?modal=admin_todo_tour">
-                            <i class="fab fa-facebook-square"></i>
+                            <i class="fab fa-facebook-square">FaceBook</i>
                         </a>
                     </li>
                 </ul>
@@ -89,3 +128,14 @@
 
     </div>
 </template>
+
+<style>
+.title-menu {
+    
+   font-size: 18px;
+    font-weight: bold;
+    border-bottom: black solid 1px;
+    padding-bottom: 6px;
+    width: 66px;
+}
+</style>
