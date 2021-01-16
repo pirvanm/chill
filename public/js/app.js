7c1d082b3c303eb0437563726a7c2a2af6c5f3dd
Your Name <pirvan.marian@gmail.com>
2021-01-16 20:54:44 +0200

    logo

--- public/js/app.js
+++ public/js/app.js
@@ -19287,8 +19287,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! F:\laragon\proj\www\chill3\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! F:\laragon\proj\www\chill3\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/chillw/dev/chill_user/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/chillw/dev/chill_user/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
