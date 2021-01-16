32f082f83f7cf5c3ae54294cfebb05de16da460e
Your Name <pirvan.marian@gmail.com>
2021-01-16 22:24:15 +0200

    update for playlist

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -32,75 +32,75 @@ beforeDestroy() {
 
    methods: {
     move(e) {
-      if (!(e.keyCode === 48 && e.ctrlKey)) {
+      if ((e.keyCode === 48 )) {
         
          window.location.href = 'https://chillwhispers.com/playlists/1'
         //return;
       }
 
-      if (!(e.keyCode === 49 && e.ctrlKey)) {
+      if (e.keyCode === 49 ) {
         console.log('stopp');
          window.location.href = 'https://chillwhispers.com/playlists/3'
         //return;
       }
 
-      if (!(e.keyCode === 50 && e.ctrlKey)) {
+      if ((e.keyCode === 50 )) {
         console.log('stopp');
          window.location.href = 'https://chillwhispers.com/playlists/4'
         //return;
       }
 
-      if (!(e.keyCode === 51 && e.ctrlKey)) {
+      if ((e.keyCode === 51)) {
         console.log('stopp');
          window.location.href = 'https://chillwhispers.com/playlists/6'
         //return;
       }
 
-      if (!(e.keyCode === 52 && e.ctrlKey)) {
+      if ((e.keyCode === 52 )) {
         console.log('stopp');
          window.location.href = 'https://chillwhispers.com/playlists/7'
         //return;
       }
 
-      if (!(e.keyCode === 53 && e.ctrlKey)) {
+      if ((e.keyCode === 53 )) {
         console.log('stopp');
          window.location.href = 'https://chillwhispers.com/playlists/8'
         //return;
       }
 
-      if (!(e.keyCode === 54 && e.ctrlKey)) {
+      if ((e.keyCode === 54 )) {
         console.log('stopp');
          window.location.href = 'https://chillwhispers.com/playlists/9'
         //return;
       }
 
-      if (!(e.keyCode === 55 && e.ctrlKey)) {
+      if ((e.keyCode === 55)) {
         console.log('stopp');
          window.location.href = 'https://chillwhispers.com/playlists/10'
         //return;
       }
 
-      if (!(e.keyCode === 56 && e.ctrlKey)) {
+      if ((e.keyCode === 56)) {
         console.log('stopp');
          window.location.href = 'https://chillwhispers.com/playlists/13'
         //return;
       }
 
-      if (!(e.keyCode === 57 && e.ctrlKey)) {
+      if ((e.keyCode === 57)) {
         console.log('stopp');
          window.location.href = 'https://chillwhispers.com/playlists/14'
         //return;
       }
 
-       if (!(e.keyCode === 17 && e.ctrlKey)) {
+       if ((e.keyCode === 17 )) {
         console.log('stopp');
-         window.location.href = 'https://chillwhispers.com/playlists/19'
+         window.location.href = 'https://chillwhispers.com/playlists/19';
         //return;
       }
 
-        if (!(e.keyCode === 84 && e.ctrlKey)) {
+        if ((e.keyCode === 84 )) {
         console.log('stopp');
-         window.location.href = 'https://chillwhispers.com/playlists/20'
+         window.location.href = 'https://chillwhispers.com/playlists/20';
         //return;
       }
 
