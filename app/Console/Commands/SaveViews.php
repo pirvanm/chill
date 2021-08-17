8ae869c41d21c49d9138554914c56b69373e42ca
imsidz <imsidzluv@gmail.com>
2021-08-17 23:07:27 +0530

    fix command

--- app/Console/Commands/SaveViews.php
+++ app/Console/Commands/SaveViews.php
@@ -40,78 +40,83 @@ class SaveViews extends Command
     public function handle()
     {
         a:
-
         $video = Video::whereNull('duration')->first();
-
         $videoApi = Youtube::getVideoInfo($video->videoId);
 
         if ($videoApi) {
             $yduration = $videoApi->contentDetails->duration;
-        if ($yduration) {
-            preg_match_all('/(\d+)/', $yduration, $parts);
+            if ($yduration) {
+                preg_match_all('/(\d+)/', $yduration, $parts);
 
-            // Put in zeros if we have less than 3 numbers.
-            if (count($parts[0]) == 1) {
-                array_unshift($parts[0], "0", "0");
-            } elseif (count($parts[0]) == 2) {
-                array_unshift($parts[0], "0");
+                // Put in zeros if we have less than 3 numbers.
+                if (count($parts[0]) == 1) {
+                    array_unshift($parts[0], "0", "0");
+                } elseif (count($parts[0]) == 2) {
+                    array_unshift($parts[0], "0");
+                }
+
+                $sec_init = $parts[0][2];
+                $seconds = $sec_init % 60;
+                $seconds_overflow = floor($sec_init / 60);
+
+                $min_init = $parts[0][1] + $seconds_overflow;
+                $minutes = ($min_init) % 60;
+                $minutes_overflow = floor(($min_init) / 60);
+
+                $hours = $parts[0][0] + $minutes_overflow;
+
+                if ($hours != 0)
+                    $duration = $hours . ':' . $minutes . ':' . $seconds;
+                else
+                    $duration = '00' . ':'  . $minutes . ':' . $seconds;
+            } else {
+                $duration = '00:00:00';
             }
 
-            $sec_init = $parts[0][2];
-            $seconds = $sec_init % 60;
-            $seconds_overflow = floor($sec_init / 60);
+            $str_time = preg_replace("/^([\d]{1,2})\:([\d]{2})$/", "00:$1:$2", $duration);
 
-            $min_init = $parts[0][1] + $seconds_overflow;
-            $minutes = ($min_init) % 60;
-            $minutes_overflow = floor(($min_init) / 60);
+            sscanf($str_time, "%d:%d:%d", $hours, $minutes, $seconds);
 
-            $hours = $parts[0][0] + $minutes_overflow;
+            $time_seconds = $hours * 3600 + $minutes * 60 + $seconds;
 
-            if ($hours != 0)
-                $duration = $hours . ':' . $minutes . ':' . $seconds;
-            else
-                $duration = '00' . ':'  . $minutes . ':' . $seconds;
+            $durationType = 0;
+
+            if ($time_seconds < 360) {
+                $durationType = 1;
+            }
+
+            if ($time_seconds > 360 && $time_seconds < 900) {
+                $durationType = 2;
+            }
+
+            if ($time_seconds > 900 && $time_seconds < 3000) {
+                $durationType = 3;
+            }
+
+            if ($time_seconds > 3000 && $time_seconds < 7200) {
+                $durationType = 4;
+            }
+
+            if ($time_seconds > 7200) {
+                $durationType = 5;
+            }
+
+            $video->duration = $duration;
+            $video->type_duration = $durationType;
+            $video->save();
+            echo  $video->title . " \n";
+            echo " saved \n";
+
         } else {
             $duration = '00:00:00';
-        }
-
-        $str_time = preg_replace("/^([\d]{1,2})\:([\d]{2})$/", "00:$1:$2", $duration);
-
-        sscanf($str_time, "%d:%d:%d", $hours, $minutes, $seconds);
-
-        $time_seconds = $hours * 3600 + $minutes * 60 + $seconds;
-
-        $durationType = 0;
-
-        if ($time_seconds < 360) {
             $durationType = 1;
-        }
-
-        if ($time_seconds > 360 && $time_seconds < 900) {
-            $durationType = 2;
-        }
-
-        if ($time_seconds > 900 && $time_seconds < 3000) {
-            $durationType = 3;
-        }
-
-        if ($time_seconds > 3000 && $time_seconds < 7200) {
-            $durationType = 4;
-        }
-
-        if ($time_seconds > 7200) {
-            $durationType = 5;
-        }
-        $video->duration = $duration;
-        $video->type_duration = $durationType;
-        $video->save();
-
-            echo " saved \n";
-        } else {
-            $video->views = 1;
+            $video->duration = $duration;
+            $video->type_duration = $durationType;
             $video->save();
-        }
 
+            echo " failed \n";
+        }
+         echo " goto A \n";
         goto a;
     }
 }
